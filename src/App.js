860d833e9bbc313fd7e6d978d9a4b476d0e74dbf
wil <wil@wil.i.am>
2020-03-02 20:42:47 -0300

    correctoin of an issue

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import { Header } from './components/layout/Header';
 
 export const App = () => {
   return (
     <div className="App">
+	  	<Header/>
     	<p>teste de app</p>
 	</div>
   );
---
+++ src/components/layout/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export const Header = ()  => {
+	return(
+		<header>
+			<nav>
+				<div>
+					<p>this is a image</p>
+				</div>
+
+				<div>
+					<ul>
+						<li>+</li>
+						<li></li>
+					</ul>
+				</div>
+			</nav>
+		</header>
+	);
+};
